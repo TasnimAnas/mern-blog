bf4923b3600d2612e1c89e471832cda2b2464f43
Tasnim Anas <anasfarazi0151@gmail.com>
2024-03-05 03:49:40 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { About, Dashboard, Home, NotFound, SignIn, SignUp } from "./pages";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 export default App;
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import About from "./About";
+import Dashboard from "./Dashboard";
+import Home from "./Home";
+import NotFound from "./NotFound";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+
+export { About, Dashboard, Home, NotFound, SignIn, SignUp };
